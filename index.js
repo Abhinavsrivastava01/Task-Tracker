d340ef4cf30897e2375057fb9fdf76ec8c7b29f9
safar123456 <abhiav498@gamil.com>
2024-03-29 11:47:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import Connection from './database/db.js';
+import Routes from './routes/route.js';
+import path from 'path';
+const _dirname=path.resolve();
+const app = express();
+
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', Routes);
+app.use(express.static(path.join("_dirname","./client/build")));
+
+app.get('*',function(_, res){
+   res.sendFile(path.join(_dirname,"./client/build/index.html"),function(err){
+           res.status(500).send(err);
+   })
+})
+
+const PORT = process.env.PORT || 8000;
+
+Connection();
+
+app.listen(PORT, () => console.log(`Your server is running successfully on PORT ${PORT}`));
